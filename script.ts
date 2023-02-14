79ab8e4ddd6d1da7ed4eafff485d88f53562c4c5
xsahxl <xsahxl@126.com>
2023-02-14 21:58:17 +0800

    first commit

---
+++ script.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: "xsahxl",
+      email: "xsahxl@126.com",
+    },
+  });
+  console.log(user);
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
